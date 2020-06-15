828bbc132555445955032a4ef1194bfb13703daf
Cai8023 <1765116032@qq.com>
2020-06-15 13:58:22 +0800

    ck

--- config.h
+++ config.h
@@ -1,6 +1,10 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
+/**********  人员 **********/
+// ck
+
+
 /**********  游戏配置数据 **********/
 #define GAME_RES_PATH "./../practice_01_PlainWar/plane.rcc" // 编译目录下的rcc文件
 #define GAME_ICON ":/res/app.ico"   // 图标文件
