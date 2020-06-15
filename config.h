3dcd11e57bbfe5ed473120e6ca625fd31b8cecb8
YIKIYA <1090450683@qq.com>
2020-06-15 19:14:41 +0800

    add yk

--- config.h
+++ config.h
@@ -2,11 +2,12 @@
 #define CONFIG_H
 
 /**********  人员 **********/
-// ck lq
+// ck lq yk
 
 
 /**********  游戏配置数据 **********/
 #define GAME_RES_PATH "./../practice_01_PlainWar/plane.rcc" // 编译目录下的rcc文件
 #define GAME_ICON ":/res/app.ico"   // 图标文件
+
 
 #endif // CONFIG_H
