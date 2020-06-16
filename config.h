9acd61db45bf985df86a420d9a3e200f0f5975d6
YIKIYA <1090450683@qq.com>
2020-06-16 11:40:38 +0800

    add config

---
+++ config.h
@@ -0,0 +1,13 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+/**********  人员 **********/
+// ck lq yk
+
+
+/**********  游戏配置数据 **********/
+#define GAME_RES_PATH "./../game/res.rcc" // 编译目录下的rcc文件
+#define GAME_ICON ":/res/pic/tank.ico"   // 图标文件
+
+
+#endif // CONFIG_H
