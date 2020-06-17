ad14216a8c2a8fa680334a4c3fed41148dd2306a
e <1747629823@qq.com>
2020-06-17 15:14:44 +0800

    123

--- config.h
+++ config.h
@@ -8,11 +8,11 @@
 #define GAME_HEIGHT 600         // 游戏窗口高度
 
 /**********  子弹配置数据 **********/
-#define BULLET_UP_PATH "://res/pic/bullet-1.gif"
-#define BULLET_DOWN_PATH "://res/pic/bullet-3.gif"
-#define BULLET_LEFT_PATH "://res/pic/bullet-0.gif"
-#define BULLET_RIGHT_PATH "://res/pic/bullet-2.gif"
-#define BULLET_BOMB_PATH "://res/pic/bump%1.gif"
+#define BULLET_UP_PATH ":/res/pic/bullet-1.gif"
+#define BULLET_DOWN_PATH ":/res/pic/bullet-3.gif"
+#define BULLET_LEFT_PATH ":/res/pic/bullet-0.gif"
+#define BULLET_RIGHT_PATH ":/res/pic/bullet-2.gif"
+#define BULLET_BOMB_PATH ":/res/pic/bump%1.gif"
 #define BULLET_BOMB_NUM 3
 enum direct{up,right,down,left};
 
--- game.pro
+++ game.pro
@@ -42,5 +42,5 @@ qnx: target.path = /tmp/$${TARGET}/bin
 else: unix:!android: target.path = /opt/$${TARGET}/bin
 !isEmpty(target.path): INSTALLS += target
 
-RESOURCES += \
-    res.qrc
+#RESOURCES += \
+#    res.qrc
