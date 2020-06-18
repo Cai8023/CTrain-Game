1864541a5f96bc94b451dbe537ddafe102443001
Cai8023 <1765116032@qq.com>
2020-06-18 09:53:57 +0800

    修复子弹达不到墙的代码

--- config.h
+++ config.h
@@ -22,7 +22,7 @@ extern char MAP[26][27];                    // 游戏界面分为26行26列由�
 #define TIMER_PLAY 100           // play 函数计时器
 #define TIMER_ENEMYMOVE 50      // 敌机移动时间间隔
 #define TIMER_ENEMYSHOT 1000     // 敌军发射子弹时间间隔
-#define TIMER_BULLETMOVE 20      // 子弹移动
+#define TIMER_BULLETMOVE 10      // 子弹移动
 
 /**********  地图数据  **********/
 #define MAP_BACKGROUND_GRAY ":/res/pic/bg_gray.gif" // 地图灰色背景
@@ -50,7 +50,7 @@ extern char MAP[26][27];                    // 游戏界面分为26行26列由�
 #define BULLET_BOMB3_PATH ":/res/pic/bump3.gif"     //子弹爆炸图片
 //#define BULLET_BOMB_PATH ":/res/pic/bump%1.gif"     //爆炸子弹图片路径
 //#define BULLET_BOMB_NUM 3                           //爆炸的图片有三张
-#define BULLET_SPEED 30
+#define BULLET_SPEED 10
 
 /**********  敌人配置数据 **********/
 #define ENEMY_SPEED 20
